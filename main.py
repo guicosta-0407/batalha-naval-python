f34e4f15fcef70a9d248acf485e4e57a8075a73a
Guilherme da Costa Marra <guilhermemarra00@gmail.com>
2026-09-25 19:45:24 -0300

    jogo funcionando de maneira basica

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-from utils import converter_coordenada
+from jogador import Jogador
+from computador import Computador
 
-coordenada = input("Testando conversão: ")
-linha, coluna = converter_coordenada(coordenada)
 
-print(f"coordenada: {coordenada}")
-print(f"linha: {linha}")
-print(f"coluna: {coluna}")
+def jogar_partida(modo):
+
+    j1 = Jogador("Jogador 1")
+    if modo == 1:
+        j2 = Computador("Computador")
+    else:
+        j2 = Jogador("Jogador 2")
+
+    j1.tabuleiro.posicionar_frota()
+    j2.tabuleiro.posicionar_frota()
+
+    atual, outro = j1, j2
+
+    while True:
+        print(f"\n--- Vez de {atual.nome} ---")
+        print(outro.tabuleiro.como_texto(False))
+
+        linha, coluna = atual.escolher_jogada(outro)
+        resultado, navio = outro.tabuleiro.receber_tiro(linha, coluna)
+        atual.tiros_dados.add((linha, coluna))
+
+        print(f"{atual.nome} jogou e o resultado foi: {resultado}")
+
+        if outro.tabuleiro.todos_afundados():
+            print(f"\n{atual.nome} venceu!")
+            break
+
+        if resultado == "agua":
+            atual, outro = outro, atual
+
+if __name__ == "__main__":
+    jogar_partida(1)
